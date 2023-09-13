23544893a0fdc7ac22ba18119d7475e142cc1849
whereismidel <86500659+whereismidel@users.noreply.github.com>
2023-09-14 02:58:45 +0300

    Add files via upload

--- scripts_bot.js
+++ scripts_bot.js
@@ -1,4 +1,23 @@
 function clickkk(button) {
     console.log("hi");
-    window.Telegram.WebApp.sendData("Hi");
+    let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
+    
+    const token = '1543386607:AAFEvknNWJaL2Psno3GwuglS8QlHI9D_WNw'; // Замените на токен вашего бота
+    const chatId = '787943933'; 
+
+    const message = tg.initDataUnsafe.user; // Замените на текст сообщения
+
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}`;
+
+    // Отправляем POST-запрос к Telegram API
+    fetch(url, {
+    method: 'POST',
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Выводим ответ от Telegram API
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
 }
