385fd7206313f541a99a7f348900dd2a2e768c6b
whereismidel <86500659+whereismidel@users.noreply.github.com>
2023-09-14 02:14:29 +0300

    Add files via upload

--- scripts_bot.js
+++ scripts_bot.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 
 tg.expand(); //расширяем на все окно
 
-let btnED = document.getElementById("btn"); //получаем кнопку активировать/деактивировать
+let btn = document.getElementById("btn"); //получаем кнопку активировать/деактивировать
 
 btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
 	tg.sendData("some string that we need to send"); 
