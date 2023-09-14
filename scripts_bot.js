a9636a0fad8489983349a3b750bb511407b34032
whereismidel <86500659+whereismidel@users.noreply.github.com>
2023-09-14 17:46:50 +0300

    Add files via upload

--- scripts_bot.js
+++ scripts_bot.js
@@ -1,23 +1,40 @@
 function clickkk(button) {
-    console.log("hi");
     let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
-    
+
     const token = '1543386607:AAFEvknNWJaL2Psno3GwuglS8QlHI9D_WNw'; // Замените на токен вашего бота
     const chatId = '787943933'; 
 
-    const message = JSON.stringify(tg.initDataUnsafe);; // Замените на текст сообщения
+    const message = JSON.stringify(tg);; // Замените на текст сообщения
 
-    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}`;
+    sendLongMessage(token, chatId, message);
 
-    // Отправляем POST-запрос к Telegram API
-    fetch(url, {
-    method: 'POST',
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Выводим ответ от Telegram API
-    })
-    .catch(error => {
-        console.error('Ошибка:', error);
-    });
+    function sendLongMessage(token, chatId, message){
+        const maxLength = 4096; // Максимальная длина сообщения в Telegram
+
+        if (message.length <= maxLength) {
+            sendMessage(token, chatId, message);
+        } else {
+            // Разбиваем длинное сообщение на части и отправляем их поочередно
+            const chunks = message.match(new RegExp(`.{1,${maxLength}}`, 'g'));
+            chunks.forEach(chunk => {
+                sendMessage(token, chatId, chunk);
+            });
+        }
+    }
+
+    function sendMessage(token, chatId, message){
+        const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}`;
+
+        // Отправляем POST-запрос к Telegram API
+        fetch(url, {
+        method: 'POST',
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Выводим ответ от Telegram API
+        })
+        .catch(error => {
+            console.error('Ошибка:', error);
+        });
+    }
 }
